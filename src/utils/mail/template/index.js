630e81534300158f5a0e1548f860530a9e0eac1e
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-09-12 22:54:04 +0200

    commit

--- src/utils/mail/template/index.js
+++ src/utils/mail/template/index.js
@@ -1,31 +1,28 @@
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'test@example.com',
-  from: 'test@example.com', // Use the email address or domain you verified above
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+import sgMail from '@sendgrid/mail';
+
+const { SENDGRID_API_KEY, SENDGRID_EMAIL } = process.env;
+
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const defaultParams = {
+  from: 'SENDGRID_EMAIL', // Use the email address or domain you verified above
 };
-//ES6
-sgMail
-  .send(msg)
-  .then(() => {}, error => {
-    console.error(error);
 
-    if (error.response) {
-      console.error(error.response.body)
-    }
-  });
-//ES8
-(async () => {
-  try {
-    await sgMail.send(msg);
-  } catch (error) {
-    console.error(error);
+export const sendBlogPostMail = async ({ to, title, link }) => {
+    try {
+      const msg = {
+        ...defaultParams,
+        to,
+        subject: 'Your article has been published.',
+        html: BlogPostMailTemplate({ title, link }),
+      };
+      const sendgridResponse = await sgMail.send(msg);
+      console.log(sendgridResponse);
+    } catch (error) {
+      console.error(error);
 
-    if (error.response) {
-      console.error(error.response.body)
+      if (error.response) {
+        console.error(error.response.body);
+      }
     }
-  }
-})();
+  };
